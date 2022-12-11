70fd2ef409f76b8cf9db2a9851cc2a477692cebe
ShantanuSutar <92261216+ShantanuSutar@users.noreply.github.com>
2022-12-11 13:39:19 +0530

    Adding NewTodoForm
    This allows you to add a new todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,16 @@
 import { Component } from "react";
 import Todo from "./Todo";
-
+import NewTodoForm from "./NewTodoForm";
 class TodoList extends Component {
     constructor(props) {
         super(props);
         this.state = { todos: [{ task: "Eat" }, { task: "Sleep" }] };
+        this.create = this.create.bind(this);
+    }
+    create(newTodo) {
+        this.setState({
+            todos: [...this.state.todos, newTodo]
+        })
     }
     render() {
         const todos = this.state.todos.map(todo => {
@@ -13,6 +19,7 @@ class TodoList extends Component {
         return (
             < div >
                 <h2>Todo List!</h2>
+                <NewTodoForm createTodo={this.create} />
                 <ul>
                     {todos}
                 </ul>
